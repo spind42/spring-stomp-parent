ffedc904858b7fec845fb6b1a352c822d841aa23
Stephan <spind42@gmail.com>
2019-03-20 23:14:08 +0100

    initial commit

---
+++ spring-boot-autoconfigure-stomp-client/src/main/java/org/springframework/boot/autoconfigure/stomp/client/StompClientProperties.java
@@ -0,0 +1,65 @@
+package org.springframework.boot.autoconfigure.stomp.client;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("spring.stomp.client")
+public class StompClientProperties {
+
+    private String clientName;
+    private String hostname;
+    private int port;
+    private String username;
+    private String password;
+    private int heartbeat = 1000;
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public String getHostname() {
+        return hostname;
+    }
+
+    public void setHostname(String hostname) {
+        this.hostname = hostname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public int getHeartbeat() {
+        return heartbeat;
+    }
+
+    public void setHeartbeat(int heartbeat) {
+        this.heartbeat = heartbeat;
+    }
+}
+
+
+
---
+++ spring-boot-starter-stomp-client/src/main/java/at/stsp/spring/stomp/StompClientConfigurationProperties.java
@@ -0,0 +1,9 @@
+package at.stsp.spring.stomp;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties
+public class StompClientConfigurationProperties {
+
+
+}
